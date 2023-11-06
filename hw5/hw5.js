63018d888160ae51855b9d2ccebbf9d768ba6799
VladimirKarmazin <128676499+VladimirKarmazin@users.noreply.github.com>
2023-11-06 14:10:44 +0200

    Add files via upload

---
+++ hw5/hw5.js
@@ -0,0 +1,14 @@
+
+let operation = prompt("Виберіть операцію: add, sub, mult або div");
+
+let number1 = parseFloat(prompt("Введіть перше число:"));
+
+if (operation === "add") {}
+
+else if (operation === "sub") {}
+
+else if (operation === "mult") {}
+
+else if (operation === "div") {}
+
+
